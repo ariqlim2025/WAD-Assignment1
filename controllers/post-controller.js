f5de49b22c7919c5bff11e91946c6a4e7f474596
ariqisnullpingu-dot <ariq.lim@redcross.com>
2026-03-22 04:18:33 +0800

    Added mock-data JSON files, and updated home page to interact with new schema files

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -14,48 +14,37 @@ exports.showPosts = async (req, res) => {
     const posts = JSON.parse(await fs.readFile(path.join(__dirname, '../data/posts.json')));
     const comments = JSON.parse(await fs.readFile(path.join(__dirname, '../data/comments.json')));
 
-    // For each post, attach the author and count its comment
-    for (let i = 0; i < posts.length; i++) {
-        // Find the user whose _id matches the post's author ID
-        for (let j = 0; j < users.length; j++) {
-            if (users[j]._id === posts[i].author) {
+    // For each post, attach the author and count its comment and (upvotes - downvotes)
+    for (let i=0; i < posts.length; i++) {
+        // Find the user whose _id mathces the post's authors ID
+        for (let j=0; j < users.length; j++) {
+            if (users[j]._id === posts[i].authorId) {
                 posts[i].author = users[j];
+                //console.log(posts[i]);
                 break;
             }
         }
         // Count how many comments belong to this post
         let count = 0;
         for (let k = 0; k < comments.length; k++) {
-            if (comments[k].post === posts[i]._id) {
+            if (comments[k].postId === posts[i]._id) {
                 count++;
             }
         }
         posts[i].commentCount = count;
+        // console.log(posts[i]);
     }
 
-    console.log(posts);
-    console.log("here");
-    
+    // Render the home page
     res.render('home', {
+        // Pass in all the posts data (posts + comments + author)
         posts: posts,
         username: 'TheMonster112'
     })
 
-    // posts = [
-    //     {
-    //         "_id": "p001",
-    //         "title": "Just finished my WAD assignment!",
-    //         "body": "Finally done with the Express + EJS project...",
-    //         "author": {                          // was "u001", now the full user object
-    //             "_id": "u001",
-    //             "username": "TheMonster112",
-    //             "email": "monster@mail.com",
-    //             "password": "password123",
-    //             "age": 21
-    //         },
-    //         "upvotedBy": ["u002", "u003"],
-    //         "downvotedBy": [],
-    //         "createdAt": "2026-03-18T10:30:00Z",
-    //         "commentCount": 2                   // new field added by the loop
-    //     },
+    
+    // res.render('home', {
+    //     posts: posts,
+    //     username: 'TheMonster112'
+    // })
 }
