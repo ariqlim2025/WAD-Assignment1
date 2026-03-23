4d77ce2cb6f7b312169845f41f9e6bc4e5eb3217
Kraken-75 <kaventan0705@gmail.com>
2026-03-23 22:59:11 +0800

    test

--- controllers/community-controller.js
+++ controllers/community-controller.js
@@ -3,3 +3,14 @@ const User   = require('../models/User');
 const Post   = require('../models/Post');
 const Comment = require('../models/Comment');
 const Bookmark = require('../models/Bookmark');
+
+const fs = require('fs/promises');
+const path = require('path');
+
+// // Controller function to add a vote to a post
+// exports.addCommunity = (req, res) => {
+
+//     // Read the vote data
+
+//     console.log("hello community");
+// }
---
+++ routes/community-routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const communityController = require('./../controllers/community-controller');
+
+// // GET route to handle new community
+// router.get('/community', communityController.addCommunity);
+
+// // EXPORT
+// module.exports = router;
