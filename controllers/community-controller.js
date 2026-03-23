32cb757b416e699236e1e1ea3b31dbfb6b91ca05
ariqisnullpingu-dot <ariq.lim@redcross.com>
2026-03-23 23:29:27 +0800

    test

--- controllers/community-controller.js
+++ controllers/community-controller.js
@@ -1,5 +1,22 @@
-const bcrypt = require('bcrypt');
-const User   = require('../models/User');
-const Post   = require('../models/Post');
-const Comment = require('../models/Comment');
-const Bookmark = require('../models/Bookmark');
+// const bcrypt = require('bcrypt');
+const User   = require('../models/user');
+const Post   = require('../models/post');
+const Comment = require('../models/comment');
+const Bookmark = require('../models/bookmark');
+const Vote = require('../models/vote');
+const Community = require('../models/community');
+const fs = require('fs/promises');
+const path = require('path');
+
+const communities = path.join(__dirname, '../data/communities.json');
+
+// Controller function to add a community
+exports.showCreateCommunityPage = async (req, res) => {
+    res.render('createCommunity');
+}
+
+// Controller function to add a community
+exports.addCommunity = async (req, res) => {
+    const community_data = req.body;
+    console.log('community_data: ', community_data);
+}
---
+++ routes/community-routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+// add your community routes here later
+
+module.exports = router;
